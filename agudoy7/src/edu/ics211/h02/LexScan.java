518eeb78c93c3abf2f502de01ad109b94082ef94
alessandraGudoy <gudoyalessandra@gmail.com>
2023-09-02 20:34:02 -1000

    test 2 and 3 work (alphabet and if statement); need to fix test 1 and 4 (sequence of digits for multiple tokens)

--- agudoy7/src/edu/ics211/h02/LexScan.java
+++ agudoy7/src/edu/ics211/h02/LexScan.java
@@ -37,18 +37,48 @@ public class LexScan {
 			if(starter) {
 				
 				if(Character.isJavaIdentifierPart(current)) {
+
 					tmp += str.substring(i, i+1);
+					
+					/*System.out.println();
+					System.out.println(" *** BUILDING ***");
+					System.out.println("CURRENT: " + current);
+					System.out.println("tmp: " + tmp);*/
+					
 				} else {
 					result[numTokens] = tmp;
 					numTokens += 1;
 					tmp = "";
+					
+					starter = i<=str.length()-1 ? Character.isJavaIdentifierStart(current) : starter;
+					startingDigit = i<=str.length()-1 ? Character.isDigit(current) : startingDigit;
 				}
 				
 			} else if(startingDigit) {
 				
+				if(Character.isDigit(current)) {
+					
+					tmp += str.substring(i, i+1);
+					
+				} else {
+					result[numTokens] = tmp;
+					numTokens += 1;
+					tmp = "";
+					
+					starter = i<=str.length()-1 ? Character.isJavaIdentifierStart(current) : starter;
+					startingDigit = i<=str.length()-1 ? Character.isDigit(current) : startingDigit;
+				}
+				
 			} else if(Character.isWhitespace(current)) {
 				
+				starter = i<=str.length()-1 ? Character.isJavaIdentifierStart(current) : starter;
+				startingDigit = i<=str.length()-1 ? Character.isDigit(current) : startingDigit;
+				
 			} else {
+				
+				result[numTokens] = str.substring(i, i+1);
+				numTokens += 1;
+				tmp = "";
 				
 			}
 		}
@@ -57,7 +87,7 @@ public class LexScan {
 		System.out.println();
 		System.out.println(" *** PRINTING *** ");
 		System.out.println("STR: " + str);
-		System.out.println("ARR: " + Arrays.copyOf(result, numTokens));
+		System.out.println("ARR: " + Arrays.deepToString(Arrays.copyOf(result, numTokens)));
 		
 		return Arrays.copyOf(result, numTokens);
 	}
@@ -65,13 +95,18 @@ public class LexScan {
 	
 	public static boolean unitTest() {
 		
-		boolean test1 = false;
+		boolean test1 = Arrays.equals(lexicalScanner("a2 = a1 + b22;"), 
+				new String[] { "a2", "=", "a1", "+", "b22", ";"});
 		
-		boolean test2 = false;
+		boolean test2 = Arrays.equals(lexicalScanner("if (a++ < 2)"), 
+				new String[] { "if", "(", "a", "+", "+", "<", "2", ")" });
 		
-		boolean test3 = false;
+		boolean test3 = Arrays.equals(lexicalScanner("a b c d e f g h i j k l m n o p q r s t u v w x y z"), 
+				new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+						"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"});;
 		
-		boolean test4 = false;
+		boolean test4 = Arrays.equals(lexicalScanner("a18 19b 2c0 d2e f21"), 
+				new String[] { "a18", "19", "b", "2", "c0", "d2e", "f21" });;
 		
 		return test1 && test2 && test3 && test4;
 	}
