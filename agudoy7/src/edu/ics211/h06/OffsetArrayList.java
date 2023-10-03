fa91df49e45488471c3d6709f57ed0812e1e2323
alessandraGudoy <gudoyalessandra@gmail.com>
2023-10-03 09:30:13 -1000

    initial commit for hw06; basic outline done

---
+++ agudoy7/src/edu/ics211/h06/OffsetArrayList.java
@@ -0,0 +1,73 @@
+package edu.ics211.h06;
+
+/*
+ * OffsetArrayList<E>
+ * 
+ * @author Alessandra Gudoy
+ * 
+ * 
+ * 	Runtimes:			[Best Case]		[Average Case]		[Worst Case]
+ *  Add at index:
+ *  Add at end:
+ *  Remove at index:
+ */
+
+public class OffsetArrayList<E> {
+	
+	protected E[] data;
+	protected int size;
+	protected int initialCapacity;
+	
+	public OffsetArrayList() { }
+
+	
+	public OffsetArrayList(int initialCapacity, int offset) {
+		
+	}
+	
+	
+	public int size() {
+		return 0;
+	}
+	
+	
+	public int capacity() {
+		return data.length;
+	}
+	
+	
+	public int offest() {
+		return 0;
+	}
+	
+	
+	public E get(int index) {
+		return null;
+	}
+	
+	
+	public E set(int index, E element) {
+		return null;
+	}
+	
+	
+	public boolean add(E element) {
+		return false;
+	}
+	
+	
+	public void add(int index, E element) {
+		
+	}
+	
+	
+	public E remove(int index) {
+		return null;
+	}
+	
+	
+	public String toString() {
+		return "";
+	}
+	
+}
