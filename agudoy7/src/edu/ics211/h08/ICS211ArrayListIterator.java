22807828dde1a149d5de4aaf356fdfc234a8f7d0
alessandraGudoy <gudoyalessandra@gmail.com>
2023-10-24 13:57:24 -1000

    unit test for array list iterator --- working

--- agudoy7/src/edu/ics211/h08/ICS211ArrayListIterator.java
+++ agudoy7/src/edu/ics211/h08/ICS211ArrayListIterator.java
@@ -32,9 +32,35 @@ public class ICS211ArrayListIterator<E> implements Iterator<E>{
         return this.data[position++];
     }
 
-    @Override
-    public void remove() {
+    public static void main(String[] args){
+        ArrayList<String> arrList = new ArrayList<String>();
         
+        arrList.add("awit");
+        arrList.add("bahay");
+        arrList.add("kamay");
+        arrList.add("dalampasigan");
+        p(arrList);
+
+        ICS211ArrayListIterator<String> iterator = new ICS211ArrayListIterator<String>(arrList);
+        ICS211ArrayListIterator<String> iterator2 = new ICS211ArrayListIterator<String>(arrList);
+        
+        for(int i=0; i<= arrList.size(); i++){
+            try{
+                p(iterator.hasNext());
+                p(iterator.next());
+            } catch(Exception e){
+                p("Is it working?");
+            }
+        }
+
+        p(iterator2.next());
+        p(iterator2.hasNext());
+        p(arrList);
+
+    }
+
+    public static <E> void p(E print){
+        System.out.println("\n" + print);
     }
     
 }
