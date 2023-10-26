fe7627c4846c3549c33084182527d4085f5a55cf
alessandraGudoy <gudoyalessandra@gmail.com>
2023-10-26 10:15:31 -1000

    tests for ArrayListIterator and LinkedList for hw 8
    ArayListIterator still works; LinkedList able to add elements -- stil need to test remove() and internal iterator

---
+++ agudoy7/src/edu/ics211/h08/IteratorTest.java
@@ -0,0 +1,72 @@
+package edu.ics211.h08;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorTest {
+    public static void main(String[] args){
+        //arrList();
+        linked();
+
+    }
+
+    public static <E> void linked(){
+        ICS211LinkedList<String> linkedList = new ICS211LinkedList<String>();
+
+        p(linkedList.add("def"));
+        p(linkedList.add("jkl"));
+        linkedList.add(0,"abc");
+        linkedList.add(2,"ghi");
+        linkedList.add(4,"mno");
+
+        p(linkedList.toString());
+
+        for(String str : linkedList){
+            p(str);
+        }
+
+        Iterator<String> iterator = linkedList.iterator();
+        
+        for(int i=0; i <= linkedList.size; i++){
+            try{
+                p(iterator.hasNext());
+                p(iterator.next());
+            } catch(Exception e){
+                p(e);
+                p("Is it working?");
+            }
+        }
+
+    }
+
+    public static void arrList(){
+        ArrayList<String> arrList = new ArrayList<String>();
+        
+        arrList.add("awit");
+        arrList.add("bahay");
+        arrList.add("kamay");
+        arrList.add("dalampasigan");
+        p(arrList);
+
+        ICS211ArrayListIterator<String> iterator = new ICS211ArrayListIterator<String>(arrList);
+        ICS211ArrayListIterator<String> iterator2 = new ICS211ArrayListIterator<String>(arrList);
+        
+        for(int i=0; i <= arrList.size(); i++){
+            try{
+                p(iterator.hasNext());
+                p(iterator.next());
+            } catch(Exception e){
+                p(e);
+                p("Is it working?");
+            }
+        }
+
+        p(iterator2.next());
+        p(iterator2.hasNext());
+        p(arrList);
+    }
+
+    public static <E> void p(E print){
+        System.out.println("\n" + print);
+    }
+}
