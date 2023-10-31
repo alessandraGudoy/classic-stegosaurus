aef6b6933993ad3177e140247a33fc8228ff37ff
alessandraGudoy <gudoyalessandra@gmail.com>
2023-10-31 09:02:12 -1000

    starting stack calculator

---
+++ agudoy7/src/edu/ics211/h09/StackCalculator.java
@@ -0,0 +1,26 @@
+package edu.ics211.h09;
+
+/**
+ * StackCalculator
+ * 
+ * @author Alessandra Gudoy
+ */
+public class StackCalculator {
+
+    public static void main(String[] args){
+        System.out.println(calculate(args));
+    }
+    
+    public static String calculate(String[] args){
+        StringBuilder builder = new StringBuilder();
+
+        for(int i=0; i<5; i++){
+            builder.append(i);
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+
+    
+}
