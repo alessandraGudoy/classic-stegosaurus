e8a4a09634c5ec9082b846a9b406feb353ac7721
alessandraGudoy <gudoyalessandra@gmail.com>
2023-11-25 19:36:24 -1000

    hash table test

--- agudoy7/src/edu/ics211/h12/Main.java
+++ agudoy7/src/edu/ics211/h12/Main.java
@@ -1,13 +1,11 @@
 package edu.ics211.h12;
 
-import java.util.Hashtable;
-
 public class Main {
     public static void main(String[] args){
-        UHM_Hashtable<String, Integer> hashtable = new UHM_Hashtable<>(1, 0.5);
+        UHM_Hashtable<String, Integer> hashtable = new UHM_Hashtable<>(3, 0.5);
 
         hashtable.put("o", 24);
-        hashtable.put("null", null);
+        hashtable.put("null", 8);
         hashtable.put("yo", 12);
         hashtable.put("y1", 3);
 
