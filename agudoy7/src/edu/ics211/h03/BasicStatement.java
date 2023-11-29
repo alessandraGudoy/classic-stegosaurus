021fdb8e4ba5d5747bb6d82572d170f43af1cb8a
alessandraGudoy <gudoyalessandra@gmail.com>
2023-11-29 11:56:08 -1000

    BasicStatement class commented and fixed isMethodCall() -- had || instead of && when checking the last two tokens

--- agudoy7/src/edu/ics211/h03/BasicStatement.java
+++ agudoy7/src/edu/ics211/h03/BasicStatement.java
@@ -18,18 +18,37 @@ public class BasicStatement extends Statement{
 	}
 	
 
+	// Every basic statement is NOT a compound statement
 	public boolean isCompound() {
 		return false;
 	}
 	
-	
+	/**
+	 * Check if given array of tokens create a method call
+	 * Given array is a valid method call if:
+	 * 		the first token is a Java identifier, the second token is "(",
+	 * 		the second to last token is ")", and the last token is ";"
+	 * 		
+	 * @param toks, array of tokens
+	 * @return whether or not if input is a valid method call
+	 */
 	protected boolean isMethodCall(String[] toks) {
-		return isJavaIdentifier(toks[0]) && toks[1].equals("(") ||
+		return isJavaIdentifier(toks[0]) && toks[1].equals("(") &&
 				toks[toks.length-2].equals(")") && toks[toks.length-1].equals(";");
 	}
 	
+	/**
+	 * Check if given array of tokens is a valid assignment statement
+	 * Given array is a valid assignment statement if:
+	 * 		the first token is a Java identifier, the second token is "=",
+	 * 		and the last toke is ";"
+	 * 
+	 * @param toks, array of tokens
+	 * @return whether or not if input is a valid assignment statement
+	 */
 	protected boolean isAssignment(String[] toks) {
-		return isJavaIdentifier(toks[0]) && toks[1].equals("=") && toks[toks.length-1].equals(";");
+		return isJavaIdentifier(toks[0]) && toks[1].equals("=") && 
+				toks[toks.length-1].equals(";");
 	}
 
 }
