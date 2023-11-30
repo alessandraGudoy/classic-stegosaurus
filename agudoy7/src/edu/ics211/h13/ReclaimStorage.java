6a6c5ef7477646b23a8a4570d8689bde38b33ec0
alessandraGudoy <gudoyalessandra@gmail.com>
2023-11-30 10:49:54 -1000

    reclaimUnreachable() implemented -- tested and working -- needs more test cases for more elaborate graphs

--- agudoy7/src/edu/ics211/h13/ReclaimStorage.java
+++ agudoy7/src/edu/ics211/h13/ReclaimStorage.java
@@ -6,22 +6,45 @@ package edu.ics211.h13;
  * objects that are not marked as being in use. */
 
 public class ReclaimStorage {
-    /**
-     * 
-     * @param objects
-     * @param root
-     * @return
-     */
-    public static java.util.Set<Integer> reclaimUnreachable(StorageObject[] objects, int root) {
-        // TODO: unmark all the storage objects, then
-        // traverse the graph beginning from objects[root] and mark all
-        // reachable storage objects, and finally
-        // return the indices of the objects that are NOT inUse
 
-        return new java.util.HashSet<Integer>();
+
+    /**
+     * Unmark all the storage objects, then traverse the graph beginning from the given index (objects[root])
+     *      and mark all reachable storage objects. Return the indeces of the objects that are NOT inUse()
+     * @param objects, array of StorageObjects to traverse through
+     * @param root, the index of starting point of traversal
+     * @return set of indeces not in use
+     */
+    public static java.util.Set<Integer> reclaimUnreachable(StorageObject[] objects, int root) {    // TODO
+        java.util.HashSet<Integer> unreachable = new java.util.HashSet<Integer>();
+
+        StorageObject rootNode = objects[root];
+
+        for(int i=0; i<objects.length; i++){
+            objects[i].clearInUse();
+        }
+
+        help(rootNode);
+
+        for(int i=0; i<objects.length; i++){
+            if(!objects[i].isInUse()){
+                unreachable.add(i);
+            }
+        }
+
+        return unreachable;
     }
 
+    private static void help(StorageObject obj){
+        obj.markInUse();
+        for(int i=0; i<obj.numLinks(); i++){
+            if(!obj.getLink(i).isInUse()){ 
+                help(obj.getLink(i));
+            }
+        }
+    }
     public static void main(String[] unused) {
+        
         // initial unit test: so1 and so2 refer to each other, so3 refers to so1,
         // nobody refers to so4
         java.util.HashSet<StorageObject> set1 = new java.util.HashSet<>();
@@ -33,16 +56,40 @@ public class ReclaimStorage {
         set1.add(so2);
         StorageObject so3 = new StorageObject("so3", set1); // so3 -> so2 and so1
         StorageObject so4 = new StorageObject("so4", set1); // as does so4
-        StorageObject[] storageObjects = { so1, so2, so3, so4 }; // so3 @ index 2
+        StorageObject so5 = new StorageObject("so5", 0);
+        StorageObject[] storageObjects = { so1, so2, so3, so4, so5 }; // so3 @ index 2
         set1.add(so3); // set1 has so1, so2, so3
         set2.add(so4); // set2 only has so4
+        set2.add(so5);
         // traversing the graph beginning with so3 should mark so3, so1, and so2,
         // and leave so4 not inUse
         System.out.println("set1 is " + set1);
         System.out.println("set2 is " + set2);
-        System.out.println("reclaiming " + reclaimUnreachable(storageObjects, 2));
+
+        java.util.Set<Integer> unreachable = reclaimUnreachable(storageObjects, 2);
+        java.util.HashSet<StorageObject> objects = new java.util.HashSet<>();
+        System.out.println("reclaiming " + unreachable);
+
+
+        for(int x : unreachable){
+            objects.add(storageObjects[x]);
+        }
+        System.out.println(set2.equals(objects));
+        // for(){
+
+        // }
+
         assert (set2.equals(reclaimUnreachable(storageObjects, 2)));
         // TODO: additional unit tests
+
+        StorageObject one = new StorageObject("one", 0);
+        StorageObject two = new StorageObject("two", 1);
+        two.setLink(0, one);
+        StorageObject three = new StorageObject("three", 2);
+        three.setLink(0, one);
+        three.setLink(1, two);
+        // StorageObject one = new StorageObject("one", 0);
+        // StorageObject one = new StorageObject("one", 0);
 
     }
 }
